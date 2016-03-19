368e88439fc10ff4e25cda808015156c742bbe1f
kenneth Grandberry <tbkots2@yahoo.com>
2016-03-18 22:39:08 -0500

    Initial Commit

---
+++ designCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  designCode
+//
+//  Created by KENNETH GRANDBERRY on 3/18/16.
+//  Copyright © 2016 KENNETH GRANDBERRY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
